d2797c12b1999a9eef2869ca7fea59ced6778fa7
Francojimenezcopati <fjcopati@gmail.com>
2024-02-17 17:20:54 -0300

    config db

--- crud_api/settings.py
+++ crud_api/settings.py
@@ -79,23 +79,23 @@ WSGI_APPLICATION = "crud_api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "mydatabase",
-#         "USER": "mydatabaseuser",
-#         "PASSWORD": "mypassword",
-#         "HOST": "127.0.0.1",
-#         "PORT": "5432",
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": "g4GFeA13FdEBCbdC3B2-gF611bc-CBdg",
+        "HOST": "postgres.railway.internal",
+        "PORT": "5432",
+    }
+}
 
 
 # Password validation
@@ -133,8 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
